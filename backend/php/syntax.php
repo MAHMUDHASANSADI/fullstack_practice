ca2e364cc71aad03cf2ed66c41ac95a411f170b5
Mahmud Hasan <mahmudhasansadi92@gmail.com>
2025-11-23 16:33:46 +0600

    php started

---
+++ backend/php/syntax.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Syntax Testing</title>
+</head>
+<body>
+    <h1>
+        hello this is syntax test
+    </h1>
+<?php
+    echo("hello world");
+    $sadi = "mahmud hasan";
+    echo($sadi);
+?>
+</body>
+</html>
